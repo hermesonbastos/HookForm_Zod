113834b2440a07243b2c07293d5ad5e02a0f758f
hermesonbastos <hermesonbastos@gmail.com>
2023-09-23 22:46:07 -0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import "./styles/global.css";
+
+function App() {
+  return (
+    <main className="h-screen bg-zinc-50 flex items-center justify-center">
+      <form className="flex flex-col gap-4 w-full max-w-xs">
+        <div className="flex flex-col gap-1">
+          <label htmlFor="">E-mail</label>
+          <input
+            type="email"
+            name="email"
+            className="border border-zinc-200 shadow-sm rounded h-10"
+          />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label htmlFor="">Senha</label>
+          <input
+            type="password"
+            name="password"
+            className="border border-zinc-200 shadow-sm rounded h-10"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="bg-emerald-500 rounded font-semibold text-white h-10 hover:bg-emerald-600"
+        >
+          Salvar
+        </button>
+      </form>
+    </main>
+  );
+}
+
+export default App;
